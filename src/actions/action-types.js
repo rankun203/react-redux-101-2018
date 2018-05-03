a5c883a6ee81e1767ca1ace5f85f8a9f16efb48a
Kun Ran <kun@lodestreams.com>
2018-05-03 17:30:14 +0800

    feat: update

---
+++ src/actions/action-types.js
@@ -0,0 +1,6 @@
+const ACTION_TYPES = {
+  MOVIE_LIST_GET: 'MOVIE_LIST_GET',
+  MOVIES_GET: 'MOVIES_GET'
+};
+
+export default ACTION_TYPES;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from "react";
-import { render } from "react-dom";
-import Hello from "./Hello";
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './configureStore';
+import MovieList from './movie-list';
 
 const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
+  fontFamily: 'sans-serif',
+  textAlign: 'center'
 };
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing.. to see some magic happen {"\u2728"}</h2>
+    <h2>Start editing.. to see some magic happen {'\u2728'}</h2>
+    <Provider store={store}>
+      <MovieList />
+    </Provider>
   </div>
 );
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
